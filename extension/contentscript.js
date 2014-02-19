bc857b74cbd3161a6f55e611c81dd528d8529ab5
greatghoul <greatghoul@gmail.com>
2014-02-19 17:38:03 +0800

    Extend editor toolbar on click.
    When page is clicked, find actived editor and extend its toolbar.
    Insert a toolbar item called link.

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,5 +1,24 @@
+function makeToolbarItem(name, title) {
+    return '<li><a tabindex="-1" unselectable="on" class="toolbar-item ' + name 
+        + '" href="javascript:;" data-command="' + name + '" title="' + title 
+        + '"><span>Link</span></a></li>';
+}
+
+function findActiveEditor() {
+   return $('.editor-wrapper:visible');
+}
+
+function extendEditorToolbar($editor) {
+    if ($editor.is('.crx-extended')) return;
+
+    var $item1 = makeToolbarItem('link', '点击插入链接');
+    $editor.find('.editor-toolbar').find('ul').append($item1);
+    $editor.addClass('crx-extended');
+}
+
 $(function() {
-	$(document).on('clicd', function() {
-		console.log(document.location.href);
+	$(document).on('click', function() {
+	    var $editor = findActiveEditor();
+	    extendEditorToolbar($editor);
 	});
 });
