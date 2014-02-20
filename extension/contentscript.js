685b95cf628a67cfbf76b55a896f7d7243d2c506
greatghoul <greatghoul@gmail.com>
2014-02-20 17:38:42 +0800

    在评论框中加入添加插入链接的按钮。
    目前没有找到合适的图标，所以暂时不设置图标，以 Link 替代。

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,23 +1,32 @@
 function makeToolbarItem(name, title) {
-    return '<li><a tabindex="-1" unselectable="on" class="toolbar-item ' + name 
+    return $('<li><a tabindex="-1" unselectable="on" class="toolbar-item ' + name 
         + '" href="javascript:;" data-command="' + name + '" title="' + title 
-        + '"><span>Link</span></a></li>';
+        + '"><span>Link</span></a></li>');
 }
 
 function findActiveEditor() {
    return $('.editor-wrapper:visible');
 }
 
+function insertLink(evt) {
+    var link = $.trim(window.prompt('输入链接地址', ''));
+    link && document.execCommand("CreateLink", false, link);
+}
+
 function extendEditorToolbar($editor) {
     if ($editor.is('.crx-extended')) return;
 
+    var $toolbar = $editor.find('.editor-toolbar').find('ul');
+    
     var $item1 = makeToolbarItem('link', '点击插入链接');
-    $editor.find('.editor-toolbar').find('ul').append($item1);
+    $item1.appendTo($toolbar).on('click', insertLink);
+
     $editor.addClass('crx-extended');
 }
 
+
 $(function() {
-	$(document).on('click', function() {
+	$(document).on('click', '.fake-textarea', function() {
 	    var $editor = findActiveEditor();
 	    extendEditorToolbar($editor);
 	});
