85a9f85fa507d1a2b5aaf5073fb50c787798c19f
greatghoul <greatghoul@gmail.com>
2014-02-21 17:07:25 +0800

    简化添加按钮的方式，增加缩进和反缩进

--- extension/contentscript.js
+++ extension/contentscript.js
@@ -1,20 +1,33 @@
-function makeToolbarItem(name, title) {
-    return $('<li><a tabindex="-1" unselectable="on" class="toolbar-item ' + name 
-        + '" href="javascript:;" data-command="' + name + '" title="' + title 
-        + '"><span>Link</span></a></li>');
-}
-
-function makeTollbarSeparator() {
-    return $('<li><span class="separator"></span></li>');
-}
+// executeCommand 命令详见：
+// https://developer.mozilla.org/en/docs/Rich-Text_Editing_in_Mozilla
+var TPLS = {
+    BTN_LINK: ''
+        + '<li><a tabindex="-1" unselectable="on" class="toolbar-item link"'
+        + ' href="javascript:;" title="插入链接"><span>Link</span></a></li>',
+    BTN_INDENT: ''
+        + '<li><a tabindex="-1" unselectable="on" class="toolbar-item indent"'
+        + ' href="javascript:;" title="缩进"><span>缩进</span></a></li>',
+    BTN_OUTDENT: ''
+        + '<li><a tabindex="-1" unselectable="on" class="toolbar-item outdent"'
+        + ' href="javascript:;" title="取消缩进"><span>取消缩进</span></a></li>',
+    BTN_SEP: '<li><span class="separator"></span></li>',
+};
 
 function findActiveEditor() {
    return $('.editor-wrapper:visible');
 }
 
-function insertLink(evt) {
+function onCommandCreateLink(evt) {
     var link = $.trim(window.prompt('输入链接地址', ''));
     link && document.execCommand("CreateLink", false, link);
+}
+
+function onCommandIndent(evt) {
+    document.execCommand("indent", true, null);
+}
+
+function onCommandOutdent(evt) {
+    document.execCommand("outdent", true, null);
 }
 
 function extendEditorToolbar() {
@@ -22,10 +35,11 @@ function extendEditorToolbar() {
     if ($editor.is('.crx-extended')) return;
 
     var $toolbar = $editor.find('.editor-toolbar').find('ul');
-    makeTollbarSeparator().appendTo($toolbar);
+    $toolbar.append($(TPLS.BTN_SEP));
 
-    var $item1 = makeToolbarItem('link', '点击插入链接');
-    $item1.appendTo($toolbar).on('click', insertLink);
+    $(TPLS.BTN_LINK).appendTo($toolbar).on('click', onCommandCreateLink);
+    $(TPLS.BTN_INDENT).appendTo($toolbar).on('click', onCommandIndent);
+    $(TPLS.BTN_OUTDENT).appendTo($toolbar).on('click', onCommandOutdent);
 
     $editor.data('crx-extended');
 }
