53ddcb9a74b1ac85af4823ecaf2a467372de53ea
greatghoul <greatghoul@gmail.com>
2017-09-26 23:26:50 +0800

    重写 Launchpad 导航
    - 适应 Tower 调整过的链接
    - 修正刷新按钮鼠标移过时请求错误的问题

--- src/js/modules/launchpad.js
+++ src/js/modules/launchpad.js
@@ -1,63 +1,63 @@
-var $   = require('jquery');
-var api = require('../lib/tower_api');
-var tpl = require('../lib/template');
-var log = require('../lib/log');
+var $ = require('jquery')
+var api = require('../lib/tower_api')
+var tpl = require('../lib/template')
 
-function setupLaunchpad() {
-  var hiddenTeams = api.hiddenTeams();
+function setupLaunchpad () {
+  $('.tp-launchpad').remove()
 
-  $('.tp-launchpad').remove();
+  var $launchpad = $(tpl.launchpad).prependTo('body')
+  $launchpad.append(tpl.loading)
 
-  var $launchpad = $(tpl.launchpad).prependTo('body');
-  $launchpad.append(tpl.loading);
+  api.teams(function (teams) {
+    var $teams = $(tpl.teams).appendTo($launchpad)
 
-  api.teams(function(teams) {
-    var $teams = $(tpl.teams).appendTo($launchpad);
-
-    teams.each(function(team) {
+    teams.each(function (team) {
       if (!api.isTeamHidden(team.id)) {
-        $(tpl.team.assign(team)).appendTo($teams);
+        $(tpl.team.assign(team)).appendTo($teams)
       }
-    });
+    })
 
-    $('.tp-teams').removeClass('tp-hide');
-    $('.tp-launchpad .tp-loading').remove();
-  });
+    $('.tp-teams').removeClass('tp-hide')
+    $('.tp-launchpad .tp-loading').remove()
+  })
 }
 
-function loadProjects($team) {
-  api.projects($team.data('id'), function(projects) {
-    var html = projects.map(function(project) {
+function loadProjects ($team) {
+  api.projects($team.data('id'), function (projects) {
+    var html = projects.map(function (project) {
       if (api.isProjectHidden(project.id)) {
-        return '';
+        return ''
       } else {
-        return tpl.project.assign(project);
+        return tpl.project.assign(project)
       }
-    }).join('');
+    }).join('')
 
-    $team.find('.tp-projects').html(html);
-  });
+    $team.find('.tp-projects').html(html)
+  })
 }
 
-function initialize() {
-  if (!api.moduleEnabled('launchpad')) return;
-  setupLaunchpad();
+function initialize () {
+  if (!api.moduleEnabled('launchpad')) return
+  setupLaunchpad()
 
-  $(document).on('mouseenter', '.tp-team', function() {
-    $('.tp-team.active').removeClass('active');
-    $(this).addClass('active');
-    loadProjects($(this));
-  });
+  $(document).on('mouseenter', '.tp-team', function () {
+    $('.tp-team.active').removeClass('active')
+    $(this).addClass('active')
 
-  $(document).on('mouseleave', '.tp-team', function(event) {
-    $(this).removeClass('active');
-  });
+    if ($(this).find('.tp-btn-refresh').length === 0) {
+      loadProjects($(this))
+    }
+  })
 
-  $(document).on('click', '.tp-btn-refresh', function(event) {
-    api.cache.clear('project');
-    api.cache.clear('team');
-    setupLaunchpad();
-  });
+  $(document).on('mouseleave', '.tp-team', function (event) {
+    $(this).removeClass('active')
+  })
+
+  $(document).on('click', '.tp-btn-refresh', function (event) {
+    api.cache.clear('project')
+    api.cache.clear('team')
+    setupLaunchpad()
+  })
 }
 
-module.exports = initialize;
+module.exports = initialize
